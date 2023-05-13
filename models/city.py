bdedcfa6319b5a28f2856e33740cab974f0f65c0
Calvin <johncalvinkarume@gmail.com>
2023-05-13 14:39:36 -0700

    All complete Tasks

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""City model module"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City Class representation"""
+    state_id = ""
+    name = ""
